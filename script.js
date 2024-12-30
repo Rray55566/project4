437957a35b8fa241ba09b568149980483e8feec1
Rray55566 <amitt8226@gmail.com>
2024-12-30 23:10:16 +0530

    both button

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-const toggleCheckbox = document.getElementById('switch');
+const toggleCheckbox = document.getElementById("switch");
+const contentOne = document.getElementById("burner_flame");
+const tea = document.querySelector(".tea");
 
-const contentOne = document.getElementById('burner_flame');
 
- let CheckboxOn = false
+contentOne.style.display = "none";
 
-toggleCheckbox.addEventListener('change', () => {
-
+toggleCheckbox.addEventListener("change", () => {
   if (toggleCheckbox.checked) {
-
-    contentOne.style.display = 'block';
-   
-    
-
-  } else {
-
-    contentOne.style.display = 'none';
-
-
+    contentOne.style.display = "block"; 
+  } 
+  else {
+    contentOne.style.display = "none";
+    tea.classList.remove('boiling');
   }
+});
+
+
+const flameToggle = document.getElementById("switch_2");
+const smallFlame = document.querySelector(".small-flame");
+const largeFlame = document.querySelector(".large-flame");
+
+
+let highFlame = false; 
+largeFlame.style.display = "none"; 
+flameToggle.addEventListener("click", () => {
+    
+  highFlame = !highFlame;
+
+  if (highFlame) {
+    smallFlame.style.display = "none";
+    largeFlame.style.display = "block";
+    tea.classList.add("boiling"); 
+} else {
+    smallFlame.style.display = "block";
+    largeFlame.style.display = "none";
+    tea.classList.remove("boiling"); 
+    }
 });
